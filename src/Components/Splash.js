a0dade987c191f6dc1e449abd3ad6949dc9fea91
Robin Kelsick <rkelsick@bitwerx.com>
2022-06-14 16:39:22 -0400

    added hover effect to my picture

--- src/Components/Splash.js
+++ src/Components/Splash.js
@@ -1,26 +1,12 @@
-import {CodeSquare} from 'react-bootstrap-icons'
-import {Controller} from 'react-bootstrap-icons'
-import { Flag } from 'react-bootstrap-icons'
+import './Splash.css'
+import me from './Img/me.jpg'
 
+// TODO: add button that links to Resume. 
 
-export default function Splash(){
-    return(
-        <div id="splash">
-        <h1 id="Name"> I am Robin Kelsick II</h1>
-        <ul id="flip">
-            <li id='developer'>
-                Developer
-                <CodeSquare color='white' />
-            </li>
-            <li id='gamer'>
-                Gamer
-                <Controller color='white' />
-            </li>
-            <li id='marine'>
-                Marine
-                <Flag color='white' />
-            </li>
-        </ul>
-        </div>
+export default function Splash() {
+    return (
+<div className='Splash'>
+    <img className='Me' src={me} alt='me' />
+</div>
     )
 }
