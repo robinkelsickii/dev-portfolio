d9d960dbda4ce1f24f0d24ee4bc86c5bf2307ed2
Robin Kelsick <rkelsick@bitwerx.com>
2022-06-15 13:02:50 -0400

    added a picture and some text and centered them.

--- src/Components/Splash.js
+++ src/Components/Splash.js
@@ -5,8 +5,13 @@ import me from './Img/me.jpg'
 
 export default function Splash() {
     return (
-<div className='Splash'>
-    <img className='Me' src={me} alt='me' />
-</div>
+        <div className='Splash'>
+            <div className='center'>
+                <img className='Me' src={me} alt='me' /> 
+                <h3>I am Robin Kelsick II</h3>
+                <h5>Hello World! Welcome to my Website Built with React and some Bootstrap. See what else I can build and learn my story.</h5>
+                <h2>Sorry that there isn't much going on here... Yet... Tune in as I build my own websites and apps better and better than they were before.</h2>
+            </div>
+        </div>
     )
 }
