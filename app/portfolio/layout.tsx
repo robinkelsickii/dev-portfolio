90b14cc2f64ddd3f1db23fdeb51d615596424e45
robinkii <robinkelsickii@gmail.com>
2024-12-18 15:02:47 -0500

    Added Carousel

---
+++ app/portfolio/layout.tsx
@@ -0,0 +1,17 @@
+import NavBar from "../components/NavBar";
+
+export default function PortfolioLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <NavBar />
+
+      <main className="flex-1 bg-gray-100">
+        <div className="max-w-7xl mx-auto p-4">{children}</div>
+      </main>
+    </div>
+  );
+}
